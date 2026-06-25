9bde13ad5862c2c7d2098709819d76ceee1e0bd5
monishar-2815 <monishagowda2815@gmail.com>
2026-06-26 04:48:49 +0530

    1!+3!+---+n!=?

--- main.c
+++ main.c
@@ -1,17 +1,19 @@
 #include<stdio.h>
 int main()
 {
-    int i,p,n;
-    p=0;
+    int i,fact,s,n;
+    fact=1;
+    s=0;
     printf("enter n=");
     scanf("%d",&n);
     for(i=1;i<=n;i++)
     {
+        fact=fact*i;
         if(i%2!=0)
         {
-            printf("%d\n",i);
+            s=s+fact;
         }
     }
+    printf("%d",s);
     return 0;
 }
-
